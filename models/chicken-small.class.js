c974d0416130e4ffa60b91c21a7954643e6784f8
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-09-17 12:17:11 +0200

    small chicken added

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -1,17 +1,17 @@
-class Chicken extends MovableObject {
+class SmallChicken extends MovableObject {
     IMAGES_WALKING= [
-        "./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
-        "./img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
-        "./img/3_enemies_chicken/chicken_normal/1_walk/3_w.png"       
+        "./img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+        "./img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+        "./img/3_enemies_chicken/chicken_small/1_walk/3_w.png"       
     ];
     currentImage=0;
 
     constructor(){        
-        super().loadImage("./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+        super().loadImage("./img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
         this.x = 200 + Math.random() * 500; 
-        this.height=50;
-        this.width=50;
-        this.y=370;
+        this.height=30;
+        this.width=30;
+        this.y=390;
         this.loadImages(this.IMAGES_WALKING);
         this.animate();
     }
@@ -26,4 +26,4 @@ class Chicken extends MovableObject {
 
     }
 
- }
+}
