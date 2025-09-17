b7550daac0fdac2c06b1491cb5061f4dacd85625
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-09-17 11:30:24 +0200

    changes in images links

--- models/character.class.js
+++ models/character.class.js
@@ -1,18 +1,18 @@
 class Character extends MovableObject {
 
     IMAGES_WALKING = [
-            "../img/2_character_pepe/2_walk/W-21.png",
-            "../img/2_character_pepe/2_walk/W-22.png",
-            "../img/2_character_pepe/2_walk/W-23.png",
-            "../img/2_character_pepe/2_walk/W-24.png",
-            "../img/2_character_pepe/2_walk/W-25.png",
-            "../img/2_character_pepe/2_walk/W-26.png"
+            "./img/2_character_pepe/2_walk/W-21.png",
+            "./img/2_character_pepe/2_walk/W-22.png",
+            "./img/2_character_pepe/2_walk/W-23.png",
+            "./img/2_character_pepe/2_walk/W-24.png",
+            "./img/2_character_pepe/2_walk/W-25.png",
+            "./img/2_character_pepe/2_walk/W-26.png"
     ]
     currentImage = 0;
     
     constructor(x, y, img){
         super(x, y);
-        super.loadImage("../img/2_character_pepe/2_walk/W-21.png");
+        super.loadImage("./img/2_character_pepe/2_walk/W-21.png");
         this.loadImages(this.IMAGES_WALKING);
         this.animate();
     }
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,7 +2,7 @@ class Cloud extends MovableObject {
     y=20;
     height=400;
     constructor(){        
-        super().loadImage("../img/5_background/layers/4_clouds/1.png");
+        super().loadImage("./img/5_background/layers/4_clouds/1.png");
         this.x =Math.random() * 500;         
         this.width=720;
         this.animate();        
