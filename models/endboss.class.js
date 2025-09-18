93a3a4265c34b5783651b008ba9b55a75834bb62
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-09-18 09:22:20 +0200

    error fixed

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,28 +1,28 @@
 class Endboss extends MovableObject {
   IMAGES_WALKING = [
-    ".img/4_enemie_boss_chicken/2_alert/G5.png",
-    ".img/4_enemie_boss_chicken/2_alert/G6.png",
-    ".img/4_enemie_boss_chicken/2_alert/G7.png",
-    ".img/4_enemie_boss_chicken/2_alert/G8.png",
-    ".img/4_enemie_boss_chicken/2_alert/G9.png",
-    ".img/4_enemie_boss_chicken/2_alert/G10.png",
-    ".img/4_enemie_boss_chicken/2_alert/G11.png",
-    ".img/4_enemie_boss_chicken/2_alert/G12.png",
+    "./img/4_enemie_boss_chicken/2_alert/G5.png",
+    "./img/4_enemie_boss_chicken/2_alert/G6.png",
+    "./img/4_enemie_boss_chicken/2_alert/G7.png",
+    "./img/4_enemie_boss_chicken/2_alert/G8.png",
+    "./img/4_enemie_boss_chicken/2_alert/G9.png",
+    "./img/4_enemie_boss_chicken/2_alert/G10.png",
+    "./img/4_enemie_boss_chicken/2_alert/G11.png",
+    "./img/4_enemie_boss_chicken/2_alert/G12.png",
   ];
   currentImage = 0;
 
   constructor() {
     super().loadImage(this.IMAGES_WALKING[0]);
     this.loadImages(this.IMAGES_WALKING);
-    this.x = 700;
-    //this.height = 100;
-    //this.width = 100;
-    //this.y=370;    
-    //this.animate();
+    this.x = 1700;
+    this.height = 300;
+    this.width = 300;
+    this.y=140;    
+    this.animate();
   }
-//   animate() {
-//     setInterval(() => {
-//       this.playAnimation(this.IMAGES_WALKING);
-//     }, 200);
-//   }
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 200);
+  }
 }
